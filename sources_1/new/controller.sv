7b5aa9c6de29eb77f0999596ce870c700fc1573a
CLDXiang <417198670@qq.com>
2019-05-22 23:05:36 +0800

    add controller

--- sources_1/new/controller.sv
+++ sources_1/new/controller.sv
@@ -1,26 +1,24 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2019/05/22 20:44:22
-// Design Name: 
-// Module Name: controller
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module controller(
-
+    input logic clk, reset,
+    input logic [5:0] opcode, funct,
+    input logic zero,
+    output logic memtoreg, regdst, iord, pcsrc,
+    output logic [1:0] alusrcb,
+    output logic alusrca, irwrite, memwrite, pcen,
+    output logic regwrite,
+    output logic [1:0] aluop,
+    output logic [2:0] alucontrol
     );
+
+    logic branch, pcwrite;
+    logic [1:0] aluop;
+
+    maindec md(clk, reset, opcode, pcwrite, memwrite, irwrite, regwrite,
+        alusrca, branch, iord, memtoreg, regdst, alusrcb, pcsrc, aluop);
+    aludec ad(funct, aluop, alucontrol);
+
+    assign pcen = (zero & branch) | pcwrite;
+
 endmodule

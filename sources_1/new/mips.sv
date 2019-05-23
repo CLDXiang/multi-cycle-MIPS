a685fa777f8a6330d7968e8ae450e6b1926a65ba
CLDXiang <417198670@qq.com>
2019-05-23 11:34:33 +0800

    init mips & top

--- sources_1/new/mips.sv
+++ sources_1/new/mips.sv
@@ -1,26 +1,20 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2019/05/22 20:43:13
-// Design Name: 
-// Module Name: mips
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mips(
-
+        input logic clk, reset
     );
+    // TODO: separate mips and idmem
+    logic [5:0] opcode, funct;
+    logic zero;
+    logic memtoreg, regdst, iord, pcsrc;
+    logic [1:0] alusrcb;
+    logic alusrca, irwrite, memwrite, pcen;
+    logic regwrite;
+    logic [1:0] aluop;
+    logic [2:0] alucontrol;
+
+    controller c(clk, reset, opcode, funct, zero, memtoreg, regdst, iord, pcsrc,
+        alusrcb, alusrca, irwrite, memwrite, pcen, regwrite, aluop, alucontrol);
+    datapath dp(clk, reset, memtoreg, regdst, iord, pcsrc, alusrcb, alusrca,
+        irwrite, memwrite, pcen, regwrite, aluop, alucontrol, opcode, funct, zero);
 endmodule
--- sources_1/new/top.sv
+++ sources_1/new/top.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
 module top(
-
+        input logic clk, reset
     );
 endmodule
